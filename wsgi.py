9dd364c92838f4c3fbbf18734c35288532ffdba1
Nick Coghlan <ncoghlan@gmail.com>
2016-12-01 20:16:28 +1000

    Use the right command name

--- wsgi.py
+++ wsgi.py
@@ -7,7 +7,7 @@ application = Flask(__name__)
 @application.route("/")
 def check_builder():
     # No real code yet, just checking repdevtools is available
-    return sarge.get_stdout(["rpm2cpi", "--help"])
+    return sarge.get_stdout(["rpm2cpio", "--help"])
 
 if __name__ == "__main__":
     application.run()
